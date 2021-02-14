4ed7378107405542148709a3191413684890ced3
Gordian <gordian.cs@web.de>
2021-02-14 15:56:26 +0100

    Support for InternalName added

--- TimeRecorder/Models/Project.cs
+++ TimeRecorder/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 namespace TimeRecorder.Models
 {
@@ -12,11 +13,18 @@ namespace TimeRecorder.Models
             set => SetField(ref _ProjectId, value);
         }
 
+        private string _InternalName = string.Empty;
+        public string InternalName
+        {
+            get => _InternalName;
+            set => SetField(ref _InternalName, value);
+        }
+
         private string _ProjectName = string.Empty;
         public string ProjectName
         {
             get => _ProjectName;
-            set => SetField(ref _ProjectName, value);
+            set { SetField(ref _ProjectName, value); SetInternalNameFromName(value); }
         }
 
         private DateTimeOffset _LastUsed = DateTimeOffset.Now;
@@ -28,5 +36,14 @@ namespace TimeRecorder.Models
 
         public readonly static Guid PauseId = new Guid("10000000-0000-0000-0000-000000000000");
         public readonly static Guid NotAtWorkId = new Guid("20000000-0000-0000-0000-000000000000");
+
+        private void SetInternalNameFromName(string name)
+        {
+            var match = Regex.Match(name, @"([^_]+)_.*");
+            if (match.Success)
+            {
+                InternalName = match.Groups[1].Value;
+            }
+        }
     }
 }
